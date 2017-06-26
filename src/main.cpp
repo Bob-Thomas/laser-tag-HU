79456c4a97095f237072d2dca9a65daedb7a9651
Bob Thomas <bob_mathias_thomas@hotmail.com>
2017-06-26 16:44:24 +0200

    added file structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include "hwlib.hpp"
-#include "../rtos/rtos.hpp"
+#include "rtos.hpp"
+
+class dave : public rtos::task<> {
+    public:
+        dave(hwlib::target::pin_out& p): task("Main"), p(p){
+        }
+    private:
+        hwlib::target::pin_out& p;
+        void main() {
+            for(;;) {
+                hwlib::wait_ms(200);
+                p.set(0);
+                hwlib::wait_ms(200);
+                p.set(1);
+            }
+        }
+};
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
+    auto p = hwlib::target::pin_out(hwlib::target::pins::d13);
+    auto test = dave(p);
     rtos::run();
  return 0;
 }
