acb0c3773f003c7cee24641b45560ce65e956bfd
Bob Thomas <bobthomas295@gmail.com>
2017-07-04 20:53:02 +0200

    rewriting player task for manual registration

--- src/Statecontroller/player-task.cpp
+++ src/Statecontroller/player-task.cpp
@@ -1,11 +1,6 @@
 #include "player-task.hpp"
 
-<<<<<<< HEAD
-PlayerTask::PlayerTask(SoundController& sound, DisplayController& display): task("Player task"), sound(sound), display(display), shoot(this, "shoot-flag"), received(this, "received channel"), gameTimer(this, 60*rtos::s, "gametimer") {
-
-=======
 PlayerTask::PlayerTask(SoundController &sound, DisplayController &display, IrController &irTransmitter) : task("Player task"), sound(sound), display(display), irTransmitter(irTransmitter), shoot(this, "shoot-flag"), received(this, "received channel"), gameTimer(this, 60 * rtos::s, "gametimer") {
->>>>>>> working on player and receiver confirmation
 }
 
 void PlayerTask::main() {
@@ -17,98 +12,62 @@ void PlayerTask::main() {
 }
 
 void PlayerTask::init() {
-    Command c;
-    display.displayText("Starting game\nPress fire when\nthe game master\nnotifies you");
-    wait(shoot);
+    display.getWindowOstream() <<
+     "     Welcome\n" <<
+     " to LaserTak 0.1\n\n" <<
+     " Press fire to\n\n" <<
+     "   to start";
+     display.flush();
+     wait(shoot);
+     display.getWindowOstream() <<
+     "\fWaiting for\n\n" <<
+     "  player data";
+     display.flush();
+     Command c;
+     while(c.get_id() == -1) {
+         wait(received);
+         c = received.read();
+     }
+    display.getWindowOstream() <<
+     "\fPlayer data received\n" <<
+     "ID: " << c.get_id() << "\n" <<
+     "WEAPON: " << c.get_data();
+     display.flush();
+     hwlib::wait_ms(2*rtos::s);
+     display.getWindowOstream() <<
+     "\fWaiting for\n\n" <<
+     "  game data";
+     display.flush();
+    while(c.get_id() != 0) {
+         wait(received);
+         c = received.read();
+     }
+     display.getWindowOstream() <<
+     "\fTime received\n" <<
+     "Minutes: " << c.get_data() <<
+     "\n\n Wait for master";
+     display.flush();
+     wait(shoot);
+    while(c.get_id() != 0 || c.get_data() != 0) {
+         wait(received);
+         c = received.read();
+     }
+     display.getWindowOstream() << "\f\n\n  STARTING";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 5s";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 4s";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 3s";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 2s";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 1s";
+     display.flush();
+     display.getWindowOstream() << "\f\n\n 0s";
 
-    display.displayText("Waiting to\nreceive player\ndata");
-<<<<<<< HEAD
-    wait(received);
-    c = received.read();
-    data.setPlayerId(c.get_id());
-    data.setWeaponId(c.get_data());
-    display.displayText("player data received");
-    display.displayText("Waiting for time");
-    
-    wait(received);
-    c = received.read();
-    if(c.get_id() == 0) {
-        data.setTime(c.get_data());
-=======
-    while (c.get_id() == -1) {
-        wait(received);
-        c = received.read();
-    }
-    data.setPlayer(c.get_id());
-    data.setWeapon(c.get_data());
-    display.displayText("player data received");
-    while(received.get_size() == 0) {
-        irTransmitter.send(Command(0, data.getWeapon()).get_encoded());
-        irTransmitter.send(Command(0, data.getWeapon()).get_encoded());
-        irTransmitter.send(Command(0, data.getWeapon()).get_encoded());
-    }
-    display.displayText("Waiting for time");
-    while (c.get_id() != 0) {
-        wait(received);
-        c = received.read();
->>>>>>> working on player and receiver confirmation
-    }
-    data.setTime(c.get_data());
-    display.displayText("game time received");
-<<<<<<< HEAD
-=======
-    while(received.get_size() == 0) {
-        irTransmitter.send(Command(data.getPlayer(), data.getTime()).get_encoded());
-        irTransmitter.send(Command(data.getPlayer(), data.getTime()).get_encoded());
-        irTransmitter.send(Command(data.getPlayer(), data.getTime()).get_encoded());
-    }
->>>>>>> working on player and receiver confirmation
-    hwlib::wait_ms(2000);
-    char txt[] = "GAMETIME 10\n\nPlayer:  \nWeapon: id\n\nWaiting on start signal";
-    if (data.getTime() < 10) {
-        txt[9] = '0';
-        txt[10] = (char)(48 + data.getTime());
-    } else {
-        txt[9] = (char)(48 + (data.getTime() / 10));
-        txt[10] = (char)(48 + (data.getTime() % 10));
-    }
-    if (data.getPlayerId() < 10) {
-        txt[20] = '0';
-<<<<<<< HEAD
-        txt[21] = (char) (48 + data.getPlayerId());
-    } else {
-        txt[20] = (char) (48 + (data.getPlayerId() / 10));
-        txt[21] = (char) (48 + (data.getPlayerId() % 10));
-=======
-        txt[21] = (char)(48 + data.getPlayer());
-    } else {
-        txt[20] = (char)(48 + (data.getPlayer() / 10));
-        txt[21] = (char)(48 + (data.getPlayer() % 10));
->>>>>>> working on player and receiver confirmation
-    }
-    if (data.getWeaponId() < 10) {
-        txt[31] = '0';
-<<<<<<< HEAD
-        txt[32] = (char) (48 + data.getWeaponId());
-    } else {
-        txt[31] = (char) (48 + (data.getWeaponId() / 10));
-        txt[32] = (char) (48 + (data.getWeaponId() % 10));
-=======
-        txt[32] = (char)(48 + data.getWeapon());
-    } else {
-        txt[31] = (char)(48 + (data.getWeapon() / 10));
-        txt[32] = (char)(48 + (data.getWeapon() % 10));
->>>>>>> working on player and receiver confirmation
-    }
-    display.displayText(txt);
-    wait(received);
-    display.displayText("STARTING GAME in \n\n      5S");
-    display.displayText("STARTING GAME in \n\n      4S");
-    display.displayText("STARTING GAME in \n\n      3S");
-    display.displayText("STARTING GAME in \n\n      2S");
-    display.displayText("STARTING GAME in \n\n      1S");
-    display.displayText("STARTING GAME in \n\n      0S");
-    updateDisplay(true);
+
+
 }
 
 void PlayerTask::start() {
@@ -119,7 +78,6 @@ void PlayerTask::start() {
         if (event == shoot) {
             hwlib::cout << "pew\n";
             sound.play_shoot();
-<<<<<<< HEAD
             data.increaseShotsFired();
         }
         else if(event == received) {
@@ -127,32 +85,11 @@ void PlayerTask::start() {
             Command c = received.read();
             if(c.get_id() != 0 && c.get_id() != data.getPlayerId()) {
                 data.insertHitBy(c.get_id(), c.get_data());
-=======
-            irTransmitter.send(Command(data.getPlayer(), data.getWeapon()).get_encoded());
-        } else if (event == received) {
-            hwlib::cout << "received command\n";
-            Command c = received.read();
-            if (c.get_id() != 0 && c.get_id() != data.getPlayer()) {
->>>>>>> working on player and receiver confirmation
             }
         } else if (event == gameTimer) {
             data.setTime(data.getTime() - 1);
             if (data.getTime() < 0) {
-                char txt[] = "\n\n   YOU SURVIVED \n    END SCORE \n\n      100";
-                if (data.getHealth() < 100) {
-                    if (data.getHealth() < 10) {
-                        txt[41] = '0';
-                        txt[42] = (char)(48 + data.getHealth());
-                    } else {
-                        txt[41] = (char)(48 + (data.getHealth() / 10));
-                        txt[42] = (char)(48 + (data.getHealth() % 10));
-                    }
-                    txt[43] = ' ';
-                }
-                display.displayText(txt);
 
-                // print the hit by information in console
-                hwlib::cout << "Hit by information: \n";
                 if(data.getReceivedHits() == 0) {
                     hwlib::cout << "No hit information";
                 } else {
@@ -171,45 +108,9 @@ void PlayerTask::start() {
 }
 
 void PlayerTask::end() {
-<<<<<<< HEAD
- 
-=======
->>>>>>> working on player and receiver confirmation
 }
 
 void PlayerTask::updateDisplay(bool alive) {
-    if (!alive) {
-        char txt[] = "\n\n\n   YOU'RE DEAD \n Remaining time \n\n      10";
-        if (data.getTime() < 10) {
-            txt[43] = '0';
-            txt[44] = (char)(48 + data.getTime());
-        } else {
-            txt[43] = (char)(48 + (data.getTime() / 10));
-            txt[44] = (char)(48 + (data.getTime() % 10));
-        }
-        display.displayText(txt);
-    } else {
-        char txt[] = "hp: 100\nminutes left: 00";
-        hwlib::cout << data.getTime() << "\n";
-        if (data.getTime() < 10) {
-            txt[22] = '0';
-            txt[23] = (char)(48 + data.getTime());
-        } else {
-            txt[22] = (char)(48 + (data.getTime() / 10));
-            txt[23] = (char)(48 + (data.getTime() % 10));
-        }
-        if (data.getHealth() < 100) {
-            if (data.getHealth() < 10) {
-                txt[4] = '0';
-                txt[5] = (char)(48 + data.getHealth());
-            } else {
-                txt[4] = (char)(48 + (data.getHealth() / 10));
-                txt[5] = (char)(48 + (data.getHealth() % 10));
-            }
-            txt[6] = ' ';
-        }
-        display.displayText(txt);
-    }  
 }
 
 void PlayerTask::button_pressed() {
